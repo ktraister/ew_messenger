45d44af30d0b527b715ee90b1e740198ac26d5b2
Kayleigh Traister <kayleigh.traister@gmail.com>
2023-10-02 10:56:15 -0400

    playing sounds when sending messages!

---
+++ audio.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"embed"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
+
+        //go:embed audio
+	var content embed.FS
+
+
+func playSound() {
+	f, err := content.Open("audio/warning_beep.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
+}
