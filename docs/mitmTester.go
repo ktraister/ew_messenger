c696f7ca18cafbffcd732fc0440303a26343e7e0
Kayleigh Traister <kayleigh.traister@gmail.com>
2024-01-15 20:11:32 -0500

    WIP: init MITM detection in this branch using certificate data

---
+++ docs/mitmTester.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/pem"
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Specify the domain for which you want to check the certificate
+	domain := "localhost"
+
+	ts := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	// Make a TLS connection to the specified domain
+	conn, err := tls.Dial("tcp", domain+":443", ts)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Get the peer certificate from the TLS connection
+	cert := conn.ConnectionState().PeerCertificates[0]
+
+	// Convert certificate to PEM format
+	pemBlock := &pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw}
+	pemEncoded := pem.EncodeToMemory(pemBlock)
+
+	// Print PEM encoded certificate
+	fmt.Println(string(pemEncoded))
+
+	// Print individual fields
+	fmt.Println("Serial Number:", cert.SerialNumber)
+	fmt.Println("Subject:", cert.Subject)
+	fmt.Println("Issuer:", cert.Issuer)
+	fmt.Println("Not Before:", cert.NotBefore.Format(time.RFC3339))
+	fmt.Println("Not After:", cert.NotAfter.Format(time.RFC3339))
+	fmt.Println("Key Usage:", cert.KeyUsage)
+	fmt.Println("Extended Key Usage:", cert.ExtKeyUsage)
+	fmt.Println("DNS Names:", cert.DNSNames)
+	fmt.Println("IP Addresses:", cert.IPAddresses)
+	fmt.Println("Signature Algorithm:", cert.SignatureAlgorithm)
+	fmt.Println("Public Key Algorithm:", cert.PublicKeyAlgorithm)
+	fmt.Println("Public Key:", cert.PublicKey)
+}
--- mitm.go
+++ mitm.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
+	"fmt"
 	"fyne.io/fyne/v2/widget"
 	"github.com/sirupsen/logrus"
+	"strings"
+	"time"
 )
 
 func mitmStatusCheck(logger *logrus.Logger) {
@@ -20,5 +24,48 @@ func mitmStatusCheck(logger *logrus.Logger) {
 		return
 	}
 
+	//we up
 	statusMsgChan <- statusMsg{Target: "MITM", Text: "GO", Import: widget.SuccessImportance, Warn: ""}
+
+	ts := tlsConfig(globalConfig.RandomURL)
+	health_url := fmt.Sprintf("%s%s", strings.Replace(globalConfig.RandomURL, "api", "ws", -1), "/healthcheck")
+
+	for {
+		time.Sleep(10 * time.Second)
+
+		// Make a TLS connection to the specified domain
+		conn, err := tls.Dial("tcp", health_url, ts)
+		if err != nil {
+			logger.Error("MITM Error dialing:", err)
+			statusMsgChan <- statusMsg{Target: "MITM", Text: "WARN", Import: widget.WarningImportance, Warn: "Possible MITM attack detected"}
+			continue
+		}
+		defer conn.Close()
+
+		// Get the peer certificate from the TLS connection
+		cert := conn.ConnectionState().PeerCertificates[0]
+
+		if globalConfig.CertData.Subject != fmt.Sprintf("%s", cert.Subject) {
+			logger.Warn("MITM switch tripped")
+			statusMsgChan <- statusMsg{Target: "MITM", Text: "WARN", Import: widget.WarningImportance, Warn: "Possible MITM attack detected"}
+		}
+
+		time.Sleep(10 * time.Second)
+
+		/*
+			// Print individual fields
+			fmt.Println("Serial Number:", cert.SerialNumber)
+			fmt.Println("Subject:", cert.Subject)
+			fmt.Println("Issuer:", cert.Issuer)
+			fmt.Println("Not Before:", cert.NotBefore.Format(time.RFC3339))
+			fmt.Println("Not After:", cert.NotAfter.Format(time.RFC3339))
+			fmt.Println("Key Usage:", cert.KeyUsage)
+			fmt.Println("Extended Key Usage:", cert.ExtKeyUsage)
+			fmt.Println("DNS Names:", cert.DNSNames)
+			fmt.Println("IP Addresses:", cert.IPAddresses)
+			fmt.Println("Signature Algorithm:", cert.SignatureAlgorithm)
+			fmt.Println("Public Key Algorithm:", cert.PublicKeyAlgorithm)
+			fmt.Println("Public Key:", cert.PublicKey)
+		*/
+	}
 }
