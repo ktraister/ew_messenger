a6fe80549146295c7ee000fde5563bf240be3bba
Kayleigh Traister <kayleigh.traister@gmail.com>
2024-01-18 16:58:09 -0500

    updated config testing values and fmt'd

--- config_test.go
+++ config_test.go
@@ -15,11 +15,11 @@ func TestFetchConfig(t *testing.T) {
 		LogLevel:    "Error",
 		BinVersion:  "REPLACEME",
 		CertData: CertData{
-			"CN=Deathscyth,OU=XXXG-01D,O=OperationMeteor,L=ESUN,ST=SancKingdom,C=EW",
-			"CN=Deathscyth,OU=XXXG-01D,O=OperationMeteor,L=ESUN,ST=SancKingdom,C=EW",
-			"",
+			"CN=endlesswaltz.xyz",
+			"CN=R3,O=Let's Encrypt,C=US",
+			"[*.endlesswaltz.xyz endlesswaltz.xyz]",
 			"SHA256-RSA",
-			"RSA",
+			"ECDSA",
 		},
 	}
 
